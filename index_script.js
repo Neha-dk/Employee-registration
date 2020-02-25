95f0f6ecbe64af0d547ac43d570a29424a57d899
neha-d <nduggirala@cardlytic.com>
2020-02-25 14:47:48 +0530

    Working with table addition of employee details

--- index_script.js
+++
@@ -1,4 +0,0 @@
-function onSubmit()
-{
-    alert("Hey");
-}
--- table_script.js
+++ table_script.js
@@ -1,15 +1,34 @@
+employee=[  {
+    "FirstName": "Haritha",
+    "LastName": "Vasanthula",
+    "Email": "vrharitha1998@gmail.com",
+    "Password": "asdfgh",
+    "Dob": "12-05-1998",
+    "Gender": "female",
+    "Hobbies": ["Story Writing", "Singing"],
+    "Country": "India",
+    "State": "Andhra Pradesh",
+    "Address": "Vizag 530046"
+}]
+
+
 function loadDoc() {
+    console.log("LoadDoc()");
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            var data = JSON.parse(this.responseText);
+             data = JSON.parse(this.responseText);
             table(data);
+
         }
     }
     xhttp.open("GET", "user_data.json", true);
     xhttp.send();
+
 }
+
 function table(data) {
+    console.log("table(data)");
     var heading = "<table><tr>";
     for (let elements in data[0]) {
         heading = heading + "<th>" + elements + "</th>";
@@ -18,7 +37,6 @@ function table(data) {
     display = "";
     for (let index in data) 
     {
-        console.log(index);
         display = display+"<tr>";
         for (let property in data[index]) 
         {
@@ -28,5 +46,22 @@ function table(data) {
     }
     final = heading + display + "</table>";
     document.getElementById("table_id").innerHTML = final;
+    console.log(final);
 }
 
+function add_employee()
+{
+    console.log("add_employee()");
+
+    var new_employee = Object.create(employee);
+
+    for(var property in employee[0])
+    {
+       new_employee[property] = document.getElementsByName(property)[0].value;
+       
+    }
+    data.push(new_employee);
+    console.log(data);
+    table(data);
+
+}
